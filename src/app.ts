9f8d01ada849f8ebf4f34ce818e90299d372b082
Delaney Sylvans <delaneysylvans@gmail.com>
2025-04-03 00:41:29 +0100

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import { getUserTeam } from "./fetchFPLData";
 import {analyseTeam} from "./analyseTeam";
+import {getFPLAdvice} from "./bedrock";
+import {fetchAndStoreFPLData} from "./dynamo";
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/team', async (req, res, next) => {
     const { teamId, cookie } = req.body;
@@ -50,6 +54,26 @@ app.post('/simple-analysis-multiple', async (req, res, next) => {
     } catch (error) {
         next(error);
     }
+});
+
+app.post("/recommend", async (req, res, next) => {
+    const { teamId, cookie } = req.body;
+
+    if (!teamId || !cookie) {
+        res.status(400).json({error: "Team ID or cookie is invalid"});
+    }
+
+    try {
+        const recommendations = await getFPLAdvice(teamId, cookie);
+        res.json({ recommendations });
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.post("/update-dynamo", async (req, res) => {
+    await fetchAndStoreFPLData();
+    res.json({ success: true });
 });
 
 export default app;
--- src/config.ts
+++ src/config.ts
@@ -4,10 +4,18 @@ dotenv.config();
 
 interface Config {
     port: number;
+    fplBaseURL: string;
+    awsAccessKey: string;
+    awsSecretKey: string;
+    awsRegion: string;
 }
 
 const config: Config = {
     port: Number(process.env.PORT) || 3000,
+    awsAccessKey: process.env.AWS_ACCESS_KEY_ID!,
+    awsSecretKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    awsRegion: process.env.AWS_REGION!,
+    fplBaseURL: process.env.FPL_BASE_URL!,
 }
 
 export default config;
